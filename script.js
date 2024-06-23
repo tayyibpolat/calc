526112ff98b285c82685e68794c80c016bc00b13
Tayyib Polat <tayyibpolat@gmx.de>
2024-06-23 19:52:22 +0200

    Finished the calculator layout

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 function add(a, b){
     return a + b;
-}
+};
 function substract(a, b){
     return a - b;
-}
+};
 function multiply(a, b){
     return a * b;
-}
+};
 function divide(a, b){
     return a / b;
-}
+};
+
+let firstNum, secNum, mixer;
+
+function operator(a, b, operation){
+    if (operation === "+") return add(a, b);
+    if (operation === "-") return substract(a, b);
+    if (operation === "*") return multiply(a, b);
+    if (operation === "/") return divide(a, b);
+};
+console.log(operator(4,4,"+"));
