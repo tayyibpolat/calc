a6c4fe2f6b83aba0ab6e9ae21bce37f1a63c562a
Tayyib Polat <129407532+tayyibpolat@users.noreply.github.com>
2024-06-24 13:31:21 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function add(a, b){
+    return a + b;
+};
+function substract(a, b){
+    return a - b;
+};
+function multiply(a, b){
+    return a * b;
+};
+function divide(a, b){
+    return a / b;
+};
+
+let firstNum, secNum, mixer;
+
+function operator(a, b, operation){
+    if (operation === "+") return add(a, b);
+    if (operation === "-") return substract(a, b);
+    if (operation === "*") return multiply(a, b);
+    if (operation === "/") return divide(a, b);
+};
+let item = document.querySelectorAll(".item");
+item.addEventListener("click", function calc(num){
+    
+})
