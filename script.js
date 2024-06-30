ef97c583d70c93c956c57d54fe1dbb0303aea131
Tayyib Polat <tayyibpolat@gmx.de>
2024-06-30 16:59:26 +0200

    Added eventListener for clear and nums

--- script.js
+++ script.js
@@ -19,4 +19,15 @@ function operator(a, b, operation){
     if (operation === "*") return multiply(a, b);
     if (operation === "/") return divide(a, b);
 };
-let item = document.querySelectorAll(".item");
+
+const container = document.querySelector(".container");
+
+const inputField = document.querySelector(".input");
+
+container.addEventListener("click", (event) => {
+    if (event.target.classList.contains("num")) {
+        inputField.value += event.target.textContent;
+    } else if (event.target.classList.contains("clear")) {
+        inputField.value = "";
+    };
+});
